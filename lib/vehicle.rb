be0f46ca2d55be5ecc86d708b22a340b82ef1619
Joshua Sanders <ninjosh2@gmail.com>
2020-01-10 01:37:16 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -7,8 +7,11 @@ class Vehicle
     @wheel_number = wheel_number
   end 
   
-  def go "vrrrrrrrooom!"
+  def go 
+    "vrrrrrrrooom!"
   end 
   
-
+  def fill_up_tank 
+    "filling up!"
+  end 
 end
